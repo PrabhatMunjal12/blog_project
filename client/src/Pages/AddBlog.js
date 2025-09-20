51f060df0e9c632cc92486825aee22bcf7549cd7
Prabhat Munjal <prabhatmunjal74@gmail.com>
2025-09-20 11:05:06 +0530

    Update AddBlog.js

--- client/src/Pages/AddBlog.js
+++ client/src/Pages/AddBlog.js
@@ -35,13 +35,11 @@ const AddBlog = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("https://blog-project-5xqq.onrender.com/api/v1/add/blog", formdata,
-                {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem("token")}`
-                    },
-                }
-            );
+        const res = await axios.post(
+    "https://blog-project-5xqq.onrender.com/api/v1/user/register",
+    input,
+    { headers: { "Content-Type": "application/json" }, withCredentials: true }
+);
             alert(res.data.message);
             navigate("/");
         } catch (error) {
